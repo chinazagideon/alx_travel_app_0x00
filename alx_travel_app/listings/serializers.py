2bf424b2166f08da0ed112bdfea209f9591d6af5
chinazagideon <chinazagideon264@gmail.com>
2025-07-20 18:15:30 +0100

    add serialzers

--- alx_travel_app/listings/serializers.py
+++ alx_travel_app/listings/serializers.py
@@ -6,15 +6,38 @@ from rest_framework import serializers
 from .models import Listing
 from users.serializers import UserDetailSerializer
 from addresses.serializers import AddressDetailSerializer
+from reviews.serializers import ReviewDetailSerializer
 
+field_list = [
+    'id',
+    'title',
+    'description',
+    'price',
+    'bedrooms',
+    'owner', # UserDetailSerializer
+    'address', # AddressDetailSerializer
+    'status',
+    'reviews', # ReviewDetailSerializer
+    'created_at',
+]
 class ListingSerializer(serializers.ModelSerializer):
     """
     Serializer for the listing
     """
+    reviews = ReviewDetailSerializer(many=True, read_only=True)
+    reviews_count = serializers.SerializerMethodField(read_only=True)
+    read_only_fields = ['reviews', 'reviews_count']
+
+    def get_reviews_count(self, obj):
+        """
+        Get the number of reviews for the listing
+        """
+        return obj.reviews.count()
+    
+    
     class Meta:
         model = Listing
-        fields = '__all__'  
-
+        fields = [*field_list, 'reviews', 'reviews_count']
 class ListingDetailSerializer(serializers.ModelSerializer):
     """
     Serializer for the listing detail
@@ -24,5 +47,5 @@ class ListingDetailSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Listing
-        fields = '__all__'
+        fields = field_list
         depth = 1
